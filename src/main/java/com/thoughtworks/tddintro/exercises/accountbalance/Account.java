55793044e24dfd1b6b02ba46419bf4406539d940
Rawradin <edgargdev@gmail.com>
2017-02-09 08:14:32 -0800

    factorial and accountbalance done

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account(int amount) {
+        this.balance = amount;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+        }
+    }
 }
